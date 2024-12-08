eec8da6ef83ff4c3a8602effc1da77ba4d590b06
Rahul <rahulkridhna@gmail.com>
2024-12-08 18:19:45 +0530

    Create SecondLargest.py

---
+++ gfg160-problem-solving/SecondLargest.py
@@ -0,0 +1,20 @@
+'''
+Given an array of positive integers arr[], 
+return the second largest element from the array.
+If the second largest element doesn't exist then return -1.
+'''
+
+class Solution:
+    def getSecondLargest(self, arr):
+        # Code Here
+        if arr==[]:
+            return -1
+        
+        a = set(arr)
+        b = list(a)
+        b = sorted(b,reverse=True)
+        if len(b)<2:
+            return -1
+        else:
+            return b[1]
+        
