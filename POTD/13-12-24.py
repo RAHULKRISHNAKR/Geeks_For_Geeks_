ace9d7cd6fdf379a33ac67d2a949e7b82961a615
Rahul <rahulkridhna@gmail.com>
2024-12-14 14:22:09 +0530

    Create 13-12-24.py

---
+++ POTD/13-12-24.py
@@ -0,0 +1,6 @@
+'''A sorted array of distinct elements arr[] is rotated at some unknown point, the task is to find the minimum element in it. '''
+
+class Solution:
+    def findMin(self, arr):
+        #complete the function here
+        return min(arr)
