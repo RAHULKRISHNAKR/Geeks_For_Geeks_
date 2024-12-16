9b50445756f233e00ac3cea03d105704704b822f
Rahul <rahulkridhna@gmail.com>
2024-12-16 22:39:32 +0530

    Create sort012.py

---
+++ gfg160-problem-solving/sort012.py
@@ -0,0 +1,6 @@
+'''Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.'''
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr):
+        # code here
+        return arr.sort()
